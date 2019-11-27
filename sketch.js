d73043440eca3a463646777d7be2c1f477121424
Michele La Rosa <miki.larosa@hotmail.it>
2019-11-27 12:43:52 +0100

    3.0

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 var song;
-var analyzer;
 var button;
 
 
@@ -11,12 +10,16 @@ function preload() {
     bruno = loadImage("./assets/brunovespa.png");
     tg1 = loadImage("./assets/tg1.png");
 
-
 }
 
 function setup() {
 
-    createCanvas(windowWidth, windowHeight)
+    createCanvas(windowWidth, windowHeight);
+
+    slider = createSlider(0.3, 1, 0.5, 0.1);
+    slider.position(windowWidth / 2 - 50, windowHeight / 10 * 0.3);
+    slider.size(100, 50);
+    slider.style('background', '#73f9c4');
 
     button = createButton("PLAY");
     button.position(windowWidth / 2 - 50, windowHeight - 150);
@@ -50,16 +53,13 @@ function suona() {
         song.stop();
         button.html("RESTART");
     }
+
 }
 
 
 function draw() {
 
-    song.rate(0.5);
-
-    var volume = 0;
-    volume = analyzer.getLevel(); //mi calcola ogni frame il volume della canzone
-    volume = map(volume, 0, 1, 0, height); //rimappo il volume da 0 e 1 (valori correnti, o ci sarebbe o non ci sarebbe) a 0 e altezza dello schermo
+    song.rate(slider.value());
 
     fft.analyze();
     var low = fft.getEnergy('bass');
@@ -69,6 +69,16 @@ function draw() {
     image(background, windowWidth / 2, windowHeight / 2, windowWidth, windowHeight);
     image(bruno, windowWidth / 10 * 3, windowHeight / 10 * 3.3, mid * 1.8, mid * 2);
     image(tg1, windowWidth / 10 * 8, windowHeight / 10 * 5.6, low * 2.5, low * 1.5);
+
+    fill("#73f9c4");
+    textSize(20);
+    textAlign(CENTER);
+    text("MORE VAPORWAVE", width / 2 - 180, height / 10 * 0.65);
+
+    fill("#73f9c4");
+    textSize(20);
+    textAlign(CENTER);
+    text("LESS VAPORWAVE", width / 2 + 180, height / 10 * 0.65);
 
 }
 
